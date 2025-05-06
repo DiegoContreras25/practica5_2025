b56099a823cfda72bf4f2e1f6a9df6987d164d62
DiegoContreras25 <133098011+DiegoContreras25@users.noreply.github.com>
2025-05-06 20:52:58 +0200

    Add files via upload

---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+import { useSignal } from "@preact/signals";
+
+export default function Home() {
+  return (
+    <div>
+      Lista
+      <div>Titulo</div>
+      <div>Autor</div>
+      <div class="likes">Likes</div>
+    </div>
+  );
+}
